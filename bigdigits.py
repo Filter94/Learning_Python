b49d92b7c62445c82ddafe130694d0379dfac8f7
Filter94 <IamFilter94@gmail.com>
2013-07-08 16:43:30 +0700

    Firstone
    Хотел сначала почти как в книжке, но не получилось, потому что писал в
    стиле C. Пришлось так

---
+++ bigdigits.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# Copyright (c) 2008 Qtrac Ltd. All rights reserved.
+# This program or module is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version. It is provided for educational
+# purposes and is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+import sys
+
+
+Zero = ["  ***  ",
+        " *   * ",
+        "*     *",
+        "*     *",
+        "*     *",
+        " *   * ",
+        "  ***  "]
+One = [" * ",
+       "** ",
+       " * ",
+       " * ",
+       " * ",
+       " * ",
+       "***"]
+Two = [" *** ",
+       "*   *",
+       "*  * ",
+       "  *  ",
+       " *   ",
+       "*    ",
+       "*****"]
+Three = [" *** ",
+         "*   *",
+         "    *",
+         "  ** ",
+         "    *",
+         "*   *",
+         " *** "]
+Four = ["   *  ",
+        "  **  ",
+        " * *  ",
+        "*  *  ",
+        "******",
+        "   *  ",
+        "   *  "]
+Five = ["*****",
+        "*    ",
+        "*    ",
+        " *** ",
+        "    *",
+        "*   *",
+        " *** "]
+Six = [" *** ",
+       "*    ",
+       "*    ",
+       "**** ",
+       "*   *",
+       "*   *",
+       " *** "]
+Seven = ["*****",
+         "    *",
+         "   * ",
+         "  *  ",
+         " *   ",
+         "*    ",
+         "*    "]
+Eight = [" *** ",
+         "*   *",
+         "*   *",
+         " *** ",
+         "*   *",
+         "*   *",
+         " *** "]
+Nine = [" ****",
+        "*   *",
+        "*   *",
+        " ****",
+        "    *",
+        "    *",
+        "    *"]
+
+Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+
+try:
+    digits = sys.argv[1]
+    row = 0
+    while row < 7:
+        line = ""
+        column = 0
+        while column < len(digits):
+            number = int(digits[column])
+            digit = Digits[number]
+            s=str(digit[row])
+            i=0
+            while i<len(s):
+                if s[i]=='*':
+                    s = s[:i] + str(number) + s[i+1:]
+                i=i+1
+            line += s + " "
+            line +="   "
+            column += 1
+        print(line)
+        row += 1
+except IndexError:
+    print("usage: bigdigits.py <number>")
+except ValueError as err:
+    print(err, "in", digits)
